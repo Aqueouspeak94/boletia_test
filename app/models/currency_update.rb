b725601f142c91752dc890ae3c1afc09f878ce18
Jose Orduno <chematj010494@gmail.com>
2024-01-04 00:10:52 -0800

    added service to copulate currencies table with data from api currency

--- app/models/currency_update.rb
+++ app/models/currency_update.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# currency update model
 class CurrencyUpdate < ApplicationRecord
+  has_many :currencies
+
+  accepts_nested_attributes_for :currencies
 end
--- app/services/currency_service.rb
+++ app/services/currency_service.rb
@@ -1,12 +1,35 @@
 # frozen_string_literal: true
 
+require 'json'
 # currency service
 class CurrencyService
   include HTTParty
   base_uri 'https://api.currencyapi.com/v3/latest?'
 
-  def latest_rates
-    self.class.get('/latest', query: { apikey: ENV['currency_api_key'] })
+  def latest_currencies
+    @response = self.class.get('/latest', query: { apikey: ENV['currency_api_key'] })
+    parse_currencies
   end
 
+  def parse_currencies
+    body = JSON.parse(@response.body)
+    @date = body['meta']['last_updated_at']
+    @currencies_data = body['data']
+
+    find_currency
+  end
+
+  def find_currency
+    create_currencies unless CurrencyUpdate.find_by_currency_update(@date)
+  end
+
+  def create_currencies
+    currencies = []
+    @currencies_data.each_value do |data|
+      currencies << data
+    end
+
+    currency_date = CurrencyUpdate.create!(currency_update: @date)
+    currency_date.currencies.create!(currencies)
+  end
 end
