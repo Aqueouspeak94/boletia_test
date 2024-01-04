beb7d9041b93999f81ddb1ab524abf5d75eb6db3
Jose Orduno <chematj010494@gmail.com>
2024-01-03 20:04:42 -0800

    added routes to currency endpoint and some validations

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Currencies controller
+class CurrenciesController < ApplicationController
+  def index
+    render json: 'hello world'
+  end
+end
--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# modelo de currency
 class Currency < ApplicationRecord
+  validates :code, length: { is: 3 }
   belongs_to :currency_update
 end
