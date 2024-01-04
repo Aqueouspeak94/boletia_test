70558326f3d1a6a9c17fc25567a484b61a69ce7b
Jose Orduno <chematj010494@gmail.com>
2024-01-04 06:36:16 -0800

    created index and asign params method

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,7 +2,24 @@
 
 # Currencies controller
 class CurrenciesController < ApplicationController
+  before_action :asign_params, only: [:index]
+
   def index
-    render json: 'hello world'
+    data = @currency == 'all' ? Currency.all : Currency.where(code: @currency)
+    data = data.joins(:currency_update)
+               .where(currency_updates: { currency_update: @finit..@fend })
+    if data.empty?
+      render json: { message: 'No records found' }, status: :forbidden
+    else
+      render json: data
+    end
+  end
+
+  private
+
+  def asign_params
+    @finit = params[:finit].present? ? Date.parse(params[:finit]) : CurrencyUpdate.first&.currency_update
+    @fend = params[:fend].present? ? Date.parse(params[:fend]) : CurrencyUpdate.last&.currency_update
+    @currency = params[:currency]
   end
 end
